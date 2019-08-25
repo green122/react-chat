108150bc4f2669a0660ffd7ce8367c62af987f28
green122 <gr122n@gmail.com>
2019-08-25 15:59:44 +0300

    with scroll behaviour

--- src/components/ChatApp/ChatApp.tsx
+++ src/components/ChatApp/ChatApp.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, RefObject, useEffect, useState } from "react";
 import styled from "styled-components";
 import { ChatInput } from "../ChatInput/ChatInput";
 import { IMessage, IUser } from "../../models";
@@ -8,19 +8,65 @@ const ChatView = styled.div`
   height: 100%;
 `;
 
+const ChildList = styled.div`
+  width: 400px;
+  height: 100px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  display: flex;
+  flex-direction: column-reverse;
+`;
+
+const ScrollButton = styled.button`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  border: 0;
+  cursor: pointer;
+  background: white;
+  box-shadow: 5px 5px 5px gray;
+`;
+
 interface IChatAppProps {
   messages: IMessage[];
   users: IUser[];
   sendMessage: (message: string) => void;
 }
 
-export function ChatApp(props: IChatAppProps) {    
+export function ChatApp(props: IChatAppProps) {
+  const chatViewRef: RefObject<HTMLDivElement> = useRef({} as HTMLDivElement);
+  const [newMessagesAtBottom, setNewMessagesFlag] = useState(false);
+  const refToElement = chatViewRef.current as HTMLElement;
+ 
+  useEffect(() => {
+    if (
+      refToElement.clientHeight + refToElement.scrollTop <
+      refToElement.scrollHeight
+    ) {
+      setNewMessagesFlag(true);
+    }
+  }, [props.messages.length, refToElement]);
+
+  const scrollToBottom = () => {
+    refToElement.scrollTo({
+      top: refToElement.scrollHeight,
+      behavior: "smooth"
+    });
+    setNewMessagesFlag(false);
+  };
 
   return (
     <ChatView>
-      {props.messages.map(messageEntry => (
-        <div>{messageEntry.messageText}</div>
-      ))}
+      <ChildList ref={chatViewRef}>
+        {props.messages
+          .slice()
+          .reverse()
+          .map(messageEntry => (
+            <div>{messageEntry.messageText}</div>
+          ))}
+      </ChildList>
+      <ScrollButton onClick={scrollToBottom}>+</ScrollButton>
+      {newMessagesAtBottom && <ScrollButton onClick={scrollToBottom}>+</ScrollButton>}
       <ChatInput onSubmit={props.sendMessage} />
     </ChatView>
   );
